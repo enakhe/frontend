8e6f4f585416aed1195aad6d7d7ec9961344b1d7
Izuagbe Samuel Enakhe <izuagbesamuel73@gmail.com>
2023-09-28 15:04:40 +0100

    Update main.jsx file

--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,14 @@ import { store } from './app/store';
 import { Provider } from 'react-redux'
 import './components/Styles/main.css'
 import './styles.css'
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
 		<Provider store={store}>
-			<App />
+			<BrowserRouter basename={"/frontend/"}>
+				<App />
+			</BrowserRouter>
 		</Provider>
 	</React.StrictMode>,
 )
